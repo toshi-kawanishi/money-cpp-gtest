fc6fef8f4f63836f589b404c0bfb1e20ed57030c
Toshiyuki Kawanishi <toshi.kawanishi@gmail.com>
2010-11-19 20:31:02 +0900

    [from now] 2010年 11月 19日 金曜日 20:31:02 JST
    diff --git a/MoneyTest.cpp b/MoneyTest.cpp
    index f22b626..a711a2c 100644
    --- a/MoneyTest.cpp
    +++ b/MoneyTest.cpp
    @@ -10,7 +10,7 @@ TEST_F( MoneyTest, testMultiplication )
     {
     	Dollar five( 5 );
     	Dollar product = five.times( 2 );
    -	EXPECT_TRUE( Dollar( 10 ) == product );
    +	EXPECT_TRUE( Dollar( 10 ) == five.times( 2 ) );
    
     	product = five.times( 3 );
     	EXPECT_TRUE( Dollar( 15 ) == product );

--- MoneyTest.cpp
+++ MoneyTest.cpp
@@ -10,7 +10,7 @@ TEST_F( MoneyTest, testMultiplication )
 {
 	Dollar five( 5 );
 	Dollar product = five.times( 2 );
-	EXPECT_TRUE( Dollar( 10 ) == product );
+	EXPECT_TRUE( Dollar( 10 ) == five.times( 2 ) );
 
 	product = five.times( 3 );
 	EXPECT_TRUE( Dollar( 15 ) == product );
