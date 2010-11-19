53298286ac8d51631718d352b90b0fe98c146bae
Toshiyuki Kawanishi <toshi.kawanishi@gmail.com>
2010-11-19 20:13:58 +0900

    [from now] 2010年 11月 19日 金曜日 20:13:58 JST
    diff --git a/MoneyTest.cpp b/MoneyTest.cpp
    index 699bacf..7ede86c 100644
    --- a/MoneyTest.cpp
    +++ b/MoneyTest.cpp
    @@ -9,9 +9,9 @@ class MoneyTest : public ::testing::Test
     TEST_F( MoneyTest, testMultiplication )
     {
     	Dollar five( 5 );
    -	five.times( 2 );
    -	EXPECT_EQ( 10, five.amount );
    +	Dollar product = five.times( 2 );
    +	EXPECT_EQ( 10, product.amount );
    
    -	five.times( 3 );
    -	EXPECT_EQ( 15, five.amount );
    +	product = five.times( 3 );
    +	EXPECT_EQ( 15, product.amount );
     }
    diff --git a/MoneyTest.make b/MoneyTest.make
    new file mode 100644
    index 0000000..4f44e31
    --- /dev/null
    +++ b/MoneyTest.make
    @@ -0,0 +1,2 @@
    +MoneyTest.o: MoneyTest.cpp Money.hpp
    +	g++ -O2 -c MoneyTest.cpp -o MoneyTest.o

--- MoneyTest.cpp
+++ MoneyTest.cpp
@@ -9,9 +9,9 @@ class MoneyTest : public ::testing::Test
 TEST_F( MoneyTest, testMultiplication )
 {
 	Dollar five( 5 );
-	five.times( 2 );
-	EXPECT_EQ( 10, five.amount );
+	Dollar product = five.times( 2 );
+	EXPECT_EQ( 10, product.amount );
 
-	five.times( 3 );
-	EXPECT_EQ( 15, five.amount );
+	product = five.times( 3 );
+	EXPECT_EQ( 15, product.amount );
 }
---
+++ MoneyTest.make
@@ -0,0 +1,2 @@
+MoneyTest.o: MoneyTest.cpp Money.hpp
+	g++ -O2 -c MoneyTest.cpp -o MoneyTest.o
